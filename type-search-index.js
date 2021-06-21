1540a6a1e550c78251028d4117db682ed79fb26a
Johann von Tiesenhausen <blah424@gmail.com>
2021-06-21 12:13:58 -0400

    clear repo

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"chess","l":"Board"},{"p":"chess","l":"Knight"},{"p":"chess","l":"Location"},{"p":"chess","l":"Piece"},{"p":"chess","l":"Tour"},{"p":"chess","l":"TourViewer"}]
