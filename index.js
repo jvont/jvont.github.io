52e8f96ed10db0ef163ccf639e625c64f7635d2e
Johann von Tiesenhausen <johannvont@gmail.com>
2022-06-03 18:21:16 -0400

    update site structure

--- index.js
+++ index.js
@@ -1,57 +1,49 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const Handlebars = require('handlebars');
 const sass = require('sass');
 
-const PUBLIC = `${__dirname}/public`;
+const ROOT = `${__dirname}/public`;
 
-
-const TEMPLATES = `${__dirname}/templates`;
+const POSTS = `${__dirname}/posts`;
 const STYLES = `${__dirname}/styles`;
+const TEMPLATES = `${__dirname}/templates`;
 
 
+// Register partial templates at `partialsDir` for compilation.
+async function registerPartials(partialsDir) {
+  const filenames = await fs.readdir(partialsDir);
 
-Handlebars.registerHelper('ifwith', function(context, options) {
-  if (context && Object.keys(context).length) {
-    return options.fn(context);
-  }
-});
-
-Handlebars.registerHelper('fa-convert', function(string) {
-  return string.replace(/\s/g, '-').toLowerCase()
-});
-
-function registerPartials(partialsDir) {
-  const filenames = fs.readdirSync(partialsDir);
-
-  filenames.forEach(filename => {
+  for await (const filename of filenames) {
     const parsedPath = path.parse(filename);
 
     if (parsedPath.ext === '.hbs') {
       const filepath = path.join(partialsDir, filename);
-      const template = fs.readFileSync(filepath, 'utf-8');
+      const template = await fs.readFile(filepath, 'utf-8');
 
       Handlebars.registerPartial(parsedPath.name, template);
     }
-  });
+  }
 }
 
-function compileHtml(resume) {
-  const css = sass.compile(`${__dirname}/styles/main.scss`).css;
-  const tpl = fs.readFileSync(`${__dirname}/templates/index.hbs`, 'utf-8');
-  const partialsDir = `${__dirname}/templates/partials`;
+// Parse posts content (html/markdown).
+async function parsePosts(postsDir) {
+
+}
+
+// function compileHtml(resume) {
+//   const css = sass.compile(`${__dirname}/styles/main.scss`).css;
+//   const tpl = fs.readFileSync(`${__dirname}/templates/index.hbs`, 'utf-8');
+//   const partialsDir = `${__dirname}/templates/partials`;
   
-  return Handlebars.compile(tpl)({
-    css,
-    resume
-  });
+//   return Handlebars.compile(tpl)({
+//     css,
+//     resume
+//   });
+// }
+
+async function build() {
+  // registerPartials(`${TEMPLATES}/partials`);
 }
 
-function deploy() {
-  fs.mkdir(`${__dirname}/dist`, _ => {});
-
-  fs.copyFileSync(`${__dirname}/index.html`, `${__dirname}/dist/index.html`);
-
-}
-
-deploy()
+build()
